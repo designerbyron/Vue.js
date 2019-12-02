718f293e43654576b6bf689b3a7d18daf9f86c73
Byron Vanegas <designerbyron@gmail.com>
2019-12-01 23:02:13 -0600

    Aplicacion Lista de tareas terminada y separada en Vue.component

--- v-todo/v-todo.js
+++ v-todo/v-todo.js
@@ -1,14 +1,34 @@
-const app = new Vue({
-    el:".app",
-    data:{
-        titulo: 'Lista de Tareas',
-        tareas:[
-            {texto:'Aprender Vue.js', terminada:false},
-            {texto:'Aprender Angular 2', terminada:false},
-            {texto:'Aprender Ionic 2', terminada:false},            
-        ],
-        nuevaTarea:""
-    },
+var data = {
+    titulo: 'Lista de Tareas',
+    tareas:[
+        {texto:'Aprender Vue.js', terminada:false},
+        {texto:'Aprender Angular 2', terminada:false},
+        {texto:'Aprender Ionic 2', terminada:false},            
+    ],
+    nuevaTarea:""
+}
+
+Vue.component('titulo',{
+    template:'<h1>{{titulo}}</h1>',
+    data: function(){
+        return { titulo: 'Lista de Tareas'}
+    }
+}
+)
+
+Vue.component('nueva-tarea', {
+    template:`
+    <div class="input-group">
+    <input class="form-control"
+      type="text"
+      v-model="nuevaTarea"
+      v-on:keyup.13="agregarTarea()"
+    />
+    <span class="input-group-btn">
+        <button class="btn btn-primary" v-on:click="agregarTarea()">Agregar tarea</button>
+    </span>
+  </div>
+    `,
     methods:{
         agregarTarea: function(){
             var nuevoTexto = this.nuevaTarea.trim();
@@ -18,5 +38,39 @@ const app = new Vue({
                 })
             }
             }
+    },
+    data: function(){
+        return data;
+    }
+})
+
+Vue.component('lista-tareas', {
+    template: `
+    <ul class="list-group">
+    <li v-for="(tarea, indice) of tareas" class="list-group-item mt-2" v-bind:class="{terminada: tarea.terminada}">
+      {{tarea.texto}}
+    <span class="pull-right">
+    <button v-on:click="tarea.terminada = !tarea.terminada" type="button" class="btn btn-success btn-xs">Listo</button>
+    <button v-on:click="eliminarTarea(indice)" type="button" class="btn btn-danger btn-xs">Eliminar</button>
+    </span>
+    </li>
+
+
+  </ul>
+    `,
+    data: function(){
+        return data;
+    }
+})
+
+
+const app = new Vue({
+    el:".app",
+    data:data ,  
+  
+    methods:{
+        eliminarTarea: function(indice){
+            this.tareas.splice(indice, 1);
+        }
     }
 })
